536e90adfc57733338e07eabe9b41570ef631c8c
jmuldvp <jmul.dvp@gmail.com>
2016-12-12 19:28:55 -0700

    comfirmed_at error

--- db/migrate/20161213022311_add_confirmable_to_devise.rb
+++ db/migrate/20161213022311_add_confirmable_to_devise.rb
@@ -1,4 +1,9 @@
 class AddConfirmableToDevise < ActiveRecord::Migration[5.0]
   def change
+    change_table(:users) do |t|
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
   end
 end
