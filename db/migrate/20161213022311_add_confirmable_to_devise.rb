01c0614a910b39ce80d7d708d8513b75d9d51352
jmuldvp <jmul.dvp@gmail.com>
2016-12-12 22:44:02 -0700

    pg error

--- db/migrate/20161213022311_add_confirmable_to_devise.rb
+++ db/migrate/20161213022311_add_confirmable_to_devise.rb
@@ -5,5 +5,6 @@ class AddConfirmableToDevise < ActiveRecord::Migration[5.0]
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
+    end
   end
 end
