62207872252a011c3b0de332b08e428e2df22f03
jmuldvp <jmul.dvp@gmail.com>
2016-12-22 18:11:38 -0700

    cp2 completed with testing

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:my_user) { User.create!(email: "user@bloc.com", password: "password")}
+  let(:my_list) { List.create!(name:"grocery", user: my_user)}
+
+  it { is_expected.to have_many(:items) }
+  it { is_expected.to validate_presence_of(:name) }
+
+  describe "attributes" do
+    it "has name" do
+      expect(my_list).to have_attributes( name: my_list.name )
+    end
+  end
+
 end
